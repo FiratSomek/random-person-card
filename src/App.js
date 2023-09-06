62f9cad637330098cb73b69b614f864f509c8e38
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2023-09-06 14:36:52 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//COMPONENTS
+import Header from "./components/Header";
+import UserList from "./components/UserList";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <UserList />
+    </>
+  );
+};
+
+export default App;
